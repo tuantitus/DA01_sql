73b169b939b666f7ca2e276197d107891e58590e
tuantitus <nguyenvantuan.dhkh.k36@gmail.com>
2024-07-03 22:59:50 +0700

    Update Practice_1.sql

--- Practice_1.sql
+++ Practice_1.sql
@@ -23,3 +23,26 @@ SELECT NAME FROM Employee
 WHERE salary>2000 AND months<10
 ORDER BY employee_id;
 --Bài 9:
+select product_id from Products
+where low_fats ='Y' and recyclable ='y';
+--Bài 10: 
+select name
+from Customer
+where referee_id != 2 or referee_id is null 
+--Bài 11
+select name, population, area from World
+where area >=3000000 or population >=25000000;
+--Bài 12:
+select DISTINCT author_id As ID from Views
+where author_id = viewer_id
+ORDER BY ID ASC
+--Bài 13
+SELECT assembly_step, part  FROM parts_assembly
+WHERE 	finish_date is NULL
+--Bài 14:
+select *from lyft_drivers
+where yearly_salary <=30000 or yearly_salary >=70000
+order by yearly_salary
+-- Bài 15:
+select  advertising_channel from uber_advertising
+where year =2019 and money_spent>=100000;
